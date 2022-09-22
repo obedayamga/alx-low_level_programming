75d9658a119289951b792da6e4b3721c98c92e3a
obedayamga <ayamgaobed@gmail.com>
2022-09-22 07:34:48 -0700

    correcting erorr for assig 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates the string pointed to by @src to
- * the end of the string pointed to by @dest
- *@dest: String that will append
- *@src: string that will be concatenated upon
- *
- * Return: returns pointer to @dest
+ *_strncat - concatenates two strings but add inputted number of bytes
+ *@dest: string to be appended upon
+ *@src: string to be completed at the end of dest
+ *@n : integer parameter to compare index to
+ *Return: return new concatenated string
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
-	for (index = 0; src[index]; index++)
+
+	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
